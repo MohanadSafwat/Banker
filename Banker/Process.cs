2abfa49416d9980c1ccc5441b3478c2c77966ee3
MohanadSafwat <57914175+MohanadSafwat@users.noreply.github.com>
2021-05-13 04:03:47 +0200

    Initial commit

---
+++ Banker/Process.cs
@@ -0,0 +1,45 @@
+﻿using System;
+namespace Banker
+{
+    public class Process
+    {
+        public static int numberOfResources;
+        private int[] allocation = new int[numberOfResources];
+        private int[] max = new int[numberOfResources];
+        private int[] need = new int[numberOfResources];
+        private string status;
+
+        public int[] Allocation {
+            set { allocation = value; }
+            get { return allocation; }
+        }
+        public int[] Max
+        {
+            set { max = value; }
+            get { return max; }
+        }
+        public int[] Need
+        {
+            set { need = value; }
+            get { return need; }
+        }
+        public string Status
+        {
+            set { status = value; }
+            get { return status; }
+        }
+
+        public Process()
+        {
+            status = "active";
+
+        }
+        public Process(int[] all , int[] maxin)
+        {
+            allocation = all;
+            max = maxin;
+            status = "active";
+        }
+
+    }
+}
---
+++ Banker/Resource.cs
@@ -0,0 +1,21 @@
+﻿using System;
+namespace Banker
+{
+    public class Resource
+    {
+        private int available;
+
+        public int Available
+        {
+            set { available = value; }
+            get { return available; }
+        }
+        public Resource()
+        {
+        }
+        public Resource(int avl)
+        {
+            this.available = avl;
+        }
+    }
+}
